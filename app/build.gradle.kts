76ca8e124d847191ef566097dee0d419cb66a7c4
Dinesh2811 <dineshk281198@gmail.com>
2023-12-20 15:46:23 +0530

    v1.0.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,13 +16,13 @@ android {
 
     signingConfigs {
         create("release"){
-            storeFile = file("../dinesh28-release-key.jks")
+            storeFile = file("${rootProject.projectDir}/dinesh28-release-key.jks")
             storePassword = "dinesh28Android"
             keyAlias = "dinesh28-key-alias"
             keyPassword = "dinesh28Android"
         }
         getByName("debug") {
-            storeFile = file("../dinesh28-release-key.jks")
+            storeFile = file("${rootProject.projectDir}/dinesh28-release-key.jks")
             storePassword = "dinesh28Android"
             keyAlias = "dinesh28-key-alias"
             keyPassword = "dinesh28Android"
@@ -84,15 +84,15 @@ android {
     }
     */
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
+//    compileOptions {
+//        sourceCompatibility = JavaVersion.VERSION_17
+//        targetCompatibility = JavaVersion.VERSION_17
+//    }
+//    kotlinOptions {
+//        jvmTarget = JavaVersion.VERSION_17.toString()
+//    }
     kotlin {
-        jvmToolchain(JavaVersion.VERSION_17.toString().toInt())
-    }
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
+        jvmToolchain(17)
     }
     buildFeatures {
         buildConfig = true
@@ -129,6 +129,8 @@ android {
 }
 
 dependencies {
+//    implementation("androidx.work.work-runtime-ktx:2.9.0")
+    
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.activity:activity-compose:1.8.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
